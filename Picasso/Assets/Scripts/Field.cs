11e4fbcfdf7d0d8ab5813fd5f8580294e5911d1b
D.K <alukardev@gmail.com>
2019-04-02 07:06:19 +0300

    add post effects

---
+++ Picasso/Assets/Scripts/Field.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Field : MonoBehaviour
+    {
+        public FieldSettings Settings;
+
+        private List<GameObject> _cells;
+
+        void Awake()
+        {
+            _cells = new List<GameObject>();
+        }
+
+        void Start()
+        {
+            Prepare();
+        }
+
+        private void Prepare()
+        {
+            for (int i = 0; i < Settings.Height; i++)
+            {
+                for (int j = 0; j < Settings.Width; j++)
+                {
+                    var cell = Instantiate(Settings.CellPrefab);
+                    cell.transform.position += new Vector3(Settings.CellPositionOffset * j, Settings.CellPositionOffset * i, 0);
+                    _cells.Add(cell);
+                }
+            }
+        }
+    }
+}
---
+++ Picasso/Assets/Scripts/FieldSettings.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    [CreateAssetMenu(fileName = "New Field Settings", menuName = "Picasso/Field Settings", order = 51)]
+    public class FieldSettings : ScriptableObject
+    {
+        public short Width;
+        public short Height;
+        public GameObject CellPrefab;
+        public float CellPositionOffset;
+    }
+}
